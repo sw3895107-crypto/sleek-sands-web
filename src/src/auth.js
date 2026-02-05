87b1f7b27e35395c2a400ea459349367708afbd3
sw3895107-crypto <sw3895107@gmail.com>
2026-02-04 20:40:02 -0500

    Create auth.js

---
+++ src/src/auth.js
@@ -0,0 +1,11 @@
+import { API_URL } from "./config";
+
+export async function login(email, password) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  return res.json();
+}
